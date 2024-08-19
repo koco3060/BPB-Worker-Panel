1d3d72a2e9bfbc81e81379a5234d4438c6229154
koco3060 <141702009+koco3060@users.noreply.github.com>
2024-08-19 12:36:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '3841880b-b2af-43f2-a9c0-a3ae3d3cf128';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
@@ -218,7 +218,7 @@ export default {
                         });
 
                     default:
-                        return new Response('Not found', { status: 404 });
+                        // return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
